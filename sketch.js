da83670b79af9a9684167011682a303dcfc92a9c
EivindO <e.omtveit@gmail.com>
2024-08-28 15:18:01 +0200

    laget statisk grid og linjer som skal vise maksimum. Matten går ikke helt opp der, jeg gjetter på at hvordan jeg har definert frekvens / lambda er syndebukken her men det gjenstår å se.

--- sketch.js
+++ sketch.js
@@ -3,60 +3,46 @@ let CenterX, CenterY
 let allLines = []
 let allCircles = []
 
-let sD, sW, sCy1, sCy2
-let bH
+let slitDistance, slitHeight, slitCenterY1, slitCenterY2
+let barrierHeight
+let maxRadius
 
-let speed
+// let speed
+let frequency
+
+let L
 
 function setup() {
   frameRate = 34
   CenterX = windowWidth / 2
   CenterY = windowHeight /2
-  sD = 100
-  sW = 35
-  bH = (windowHeight - (sD + sW))/2
-  sCy1 = bH + sW/2
-  sCy2 = sCy1 + sD
-  speed = 10
+  slitDistance = 100
+  slitHeight = 35
+  barrierHeight = (windowHeight - (slitDistance + slitHeight))/2
+  slitCenterY1 = barrierHeight + slitHeight/2
+  slitCenterY2 = slitCenterY1 + slitDistance
+  // speed = 10
+  maxRadius = new createVector(windowWidth/2, slitCenterY2)
+
+  frequency = 30
+  L = 500
+
+  background(225)
+  stroke(125)
+  strokeWeight(2)
+  
+  
+
+
+
   createCanvas(windowWidth, windowHeight);
   background(225)
+
+  createGrid()
+  drawMaxima(30)
 }
 
 function draw() {
-  // set backgroundcolor
-  background(225)
-  stroke(125)
-  strokeWeight(8)
-  
-  
-  
-  // iterate through allWaves and perform actions
-  
-  for (wave in allCircles) {
-    thisWave = allCircles[wave]
-    thisWave.checkAlive(wave)
-    thisWave.move()
-    thisWave.show()
-    noStroke()
-    fill(225)
-    rect(0,0, CenterX, windowHeight)
-    noFill()
-    stroke(125)
-  } 
-
- if (frameCount%17 == 0) {
-  let newWave = new lineWave(0,CenterY + CenterY/2,0,0)
-  allLines.push(newWave)
- }
-
-  for(wave in allLines) {
-    thisWave = allLines[wave]
-    
-    thisWave.checkAlive(wave)
-    thisWave.move()
-    thisWave.show()
-  }
-
   stroke(50)
   drawBarrier()
 }
@@ -64,20 +50,44 @@ function draw() {
 function drawBarrier() {
   // create barrier with slits
   strokeWeight(16)
-  line(CenterX,0,CenterX, bH)
-  line(CenterX,windowHeight,CenterX, bH + sW + sD)
-  line(CenterX, bH + sW,CenterX, windowHeight - (bH + sW))
-  
+  line(CenterX,0,CenterX, barrierHeight)
+  line(CenterX,windowHeight,CenterX, barrierHeight + slitHeight + slitDistance)
+  line(CenterX, barrierHeight + slitHeight,CenterX, windowHeight - (barrierHeight + slitHeight))
 }
 
-function mouseClicked() {
-  let newWave = new lineWave(mouseX,mouseY,mouseX,0)
-  allLines.push(newWave)
-  newWave.show()
+function drawMaxima(n) {
+  for(let i=n*-1; i<n;i++) {
+    line(CenterX, slitCenterY1,CenterX + L, (i*L*(frequency +2))/slitDistance)
+    line(CenterX, slitCenterY2,CenterX + L, (i*L*(frequency +2))/slitDistance)
+  }
+}
+
+// function mouseClicked() {
+//   let newWave = new lineWave(mouseX,mouseY,mouseX,0)
+//   allLines.push(newWave)
+//   newWave.show()
+// }
+
+function createGrid() {
+  for (let i = 0; i < maxRadius.mag(); i++) {
+    let cWave = new circularWave(windowWidth/2, slitCenterY1, slitCenterY2, i*frequency*2)
+    cWave.show()
+  }
+
+  // noStroke()
+  // fill(225)
+  // rect(0,0, CenterX, windowHeight)
+  // noFill()
+  // stroke(125)
+
+  for (let i = 0; i < windowWidth/(frequency*2); i++) {
+    let wave = new lineWave(i*frequency, 0, i*frequency, windowHeight)
+    wave.show()
+  }
 }
 
 function createCircularWave() {
-  let newCircularWave = new circularWave(CenterX, sCy1, sCy2, 0)
+  let newCircularWave = new circularWave(CenterX, slitCenterY1, slitCenterY2, 0)
   newCircularWave.show()
   allCircles.push(newCircularWave)
 }
